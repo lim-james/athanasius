421ca85fc4fd75298fe2a1c12b8d5723d5d8a16a
James <jameslimbj@gmail.com>
2025-02-28 17:51:22 +0800

    tag: increase version

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="athanasius",
-    version="0.1.0",
+    version="0.1.1",
     description="python CLI to archiving files",
     author="James",
+    url="https://github.com/lim-james/athanasius",
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=[],
@@ -14,4 +15,9 @@ setup(
             "ath=athanasius.cli:main",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
