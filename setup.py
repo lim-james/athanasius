1c4bc4230ff52c25f79744c72232589fcc9324fc
James <jameslimbj@gmail.com>
2025-02-28 18:00:11 +0800

    docs: add readme to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 # setup.py
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="athanasius",
-    version="0.1.1",
+    version="0.1.2",
     description="python CLI to archiving files",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="James",
     url="https://github.com/lim-james/athanasius",
     packages=find_packages(),
