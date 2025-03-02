91870871f7784e2f7fb641b0b14d0efa883c4be5
James <jameslimbj@gmail.com>
2025-03-02 11:14:40 +0800

    THIS IS A BAD COMMIT

--- athanasius/extractor.py
+++ athanasius/extractor.py
@@ -7,7 +7,7 @@ def extract(archive_filename):
         file_metadata = []
 
         for _ in range(num_files):
-            name_len = struct.unpack(">I", archive_file.read(4))[0]
+            name_len = struct.unpack(">R", archive_file.read(4))[0]
             name = archive_file.read(name_len).decode("utf-8")
             size = struct.unpack(">Q", archive_file.read(8))[0]
             is_empty_dir = struct.unpack(">B", archive_file.read(1))[0] == 1
